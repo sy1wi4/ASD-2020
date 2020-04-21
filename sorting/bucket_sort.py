c2692a75152294ad3c74f23c430ca2fa4ca072d5
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-04-21 23:36:32 +0200

    Create bucket_sort.py

---
+++ sorting/bucket_sort.py
@@ -0,0 +1,32 @@
+from random import randint
+
+# n no liczba kubelkow jakie tworzymy, musimy znac ja chociazby przyblizona
+
+def bucketSort(arr, n):
+    Max = max(arr)
+    buckets = [[] for _ in range(n)] 
+    
+    for i in arr:
+        # normalizacja: dzielimy elementy przez max, dzieki czemu wsystkie beda z przedzialu [0,1]
+        normNum = i/Max 
+        bucketIdx = int((n-1) * normNum) # wybieram bucket
+        buckets[bucketIdx].append(i)     # dodaje liczbe do bucketa
+    
+    for i in range(n):
+        buckets[i] = sorted(buckets[i])     
+        #sortuje obojetnie jaka metoda, bo liczba elementow jest stala
+    
+    output = []
+    
+    for i in range(n): # i-ty bucket
+        for j in range(len(buckets[i])): # j-ty element
+            output.append(buckets[i][j])
+    return output,buckets
+
+
+arr=[]
+for i in range(10):
+    arr.append(randint(0,9))
+print(arr)
+print()
+print(bucketSort(arr,3))
