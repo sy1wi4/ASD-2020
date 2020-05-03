5436aa5f841b177cfe0469d557a008377f71c1d5
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-05-03 21:39:21 +0200

    Create min_weighted_cycle.py

---
+++ graphs/min_weighted_cycle.py
@@ -0,0 +1,15 @@
+'''
+Jak znaleźć cykl o minimalnej wadze w grafie skierowanym z dodatnimi wagami?
+Zakładamy reprezentację macierzową
+'''
+
+# ALGORYTM:
+
+# GRAF RZADKI - po kolei "usuwamy" każdą krawędź (u,v), a następnie puszczamy Dijkstrę 
+# z u i do odległości jaką otrzymamy dla wierzchołka v dodajemy wagę krawędzi z u do v,
+# po przejrzeniu wszystkich krawędzi w ten sposób mamy cykl o minimalnej wadze
+# O(E^2 * logV)
+
+# GRAF GĘSTY - dla każdej pary wierzchołków szukamy sumy wag krawędzi z u do v i z v do u,
+# bierzemy najmniejszą
+# O(V^3)
