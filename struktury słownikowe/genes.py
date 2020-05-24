45639801fa9a529c67fc504d5fd7d0e6a3d77f45
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-05-24 23:22:10 +0200

    Update genes.py

--- struktury słownikowe/genes.py
+++ struktury słownikowe/genes.py
@@ -5,7 +5,7 @@ Proszę opisać algorytm, który sprawdza czy tak faktycznie jest.
 '''
 # ALGORYTM:
 '''
-Tworzymy drzewo, gdzie root ma 4 dzieci - odpowiednio G A T C. Dodajemy wszystkie istniejące sekwencje
+Tworzymy drzewo, gdzie root ma 4 dzieci, odpowiednio G A T C. Dodajemy wszystkie istniejące sekwencje
 na bieżąco sprawdzając, czy się nie powtarzają. Osiągniemy to poprzez dodanie do każdego węzła pola
 end=False, które ustawiamy na True, gdy w danym węźle kończy się jakaś sekwencja. Dzięki temu dodając 
 sekwencję, jeżeli okaże się, że nie musimy wstawiać do drzewa nowego węzła oraz kończy się
