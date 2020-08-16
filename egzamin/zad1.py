ad1260a32e4171a2d97c4a07ed30a362e2d46dc0
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-16 17:06:02 +0200

    Create zad1.py

---
+++ egzamin/zad1.py
@@ -0,0 +1,10 @@
+'''
+Pewna kraina składa się z wysp pomiędzy którymi istnieją połączenia lotnicze, promowe oraz mosty. Pomiędzy dwoma wyspami istnieje co najwyżej 
+jeden rodzaj połączenia. Koszt przelotu z wyspy na  wyspę  wynosi  8,  koszt  przeprawy  promowej  wynosi  5,  za  przejście  mostem  trzeba  
+wnieśćopłatę 1. Poszukujemy trasy z wyspy A na wyspę B, która na kolejnych wyspach zmienia środek transportu na inny oraz minimalizuje koszt 
+podróży. Dana  jest  tablica G,  określająca  koszt  połączeń  pomiędzy  wyspami.  Wartość  0  w  macierzy oznacza  brak  bezpośredniego  
+połączenia.  Proszę  zaimplementować  funkcję islands( G, A, B )zwracającą minimalny koszt podróży z wyspy A na wyspę B. Jeżeli trasa spełniająca 
+warunki zadanianie istnieje, funkcja powinna zwrócić wartość None.
+'''
+
+# TODO
