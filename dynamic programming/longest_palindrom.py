8347269950078077ccdd08c87082624f42d189ec
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-19 23:20:43 +0200

    Update longest_palindrom.py

--- dynamic programming/longest_palindrom.py
+++ dynamic programming/longest_palindrom.py
@@ -9,7 +9,8 @@ Zaczynamy z indeksami i na początku oraz j na końcu stringa. Jeśli obie liter
 podciągu o 1 i przesuwamy oba indeksy, jeśli nie, to przesuwamy indeks i lub j, w zależności, który bardziej sie opłaca. 
 '''
 
-# naiwne - wykładnicze
+
+# naiwny - wykładniczy
 def LPS(string,i,j):
     
     if i == j :
@@ -23,5 +24,39 @@ def LPS(string,i,j):
     
     else :
         return max(LPS(string,i+1,j),LPS(string,i,j-1))
+    
+    
+    
+# ze spamiętywaniem - O(n^2)
+# ulepszenie algorytmu polega na zapamiętaniu w tablicy już raz obliczonej długości 
+# najdłuższego palindromu dla danych i oraz j - dzięki temu nie liczymy wielokrotnie tego samego
 
-print(LPS("abxaaba",0,6))
+def LPS_better(string):
+    n=len(string)
+
+    memo = [[None]*n for _ in range(n)]
+
+
+    def lps(string,i,j,memo):
+
+        if i == j :
+            return 1
+
+        elif i > j :
+            return 0
+        
+        elif string[i] == string[j] :
+
+            if memo[i][j] is None:
+                memo[i][j] = 2 + lps(string,i+1,j-1,memo)
+            
+    
+        else :
+            if memo[i][j] is None :
+                memo[i][j] = max(lps(string,i+1,j,memo),lps(string,i,j-1,memo))
+        
+        
+        return memo[i][j]
+           
+
+    return lps(string,0,len(string)-1,memo)
