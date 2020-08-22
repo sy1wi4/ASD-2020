a4540fd21647a67c6b0dced882fc45f64d5fa118
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-22 18:45:55 +0200

    Create zad1.py

---
+++ kolokwia 2020/kol1/zad1.py
@@ -0,0 +1,9 @@
+'''
+Cyfra jednokrotna to taka, która występuje w danej liczbie dokładnie jeden raz. Cyfra wielokrotna to taka, która w liczbie występuje 
+więcej niż jeden raz. Mówimy, że liczba naturalna A jest ładniejsza od liczby naturalnej B jeżeli w liczbie A występuje więcej cyfr 
+jednokrotnych niż w B, a jeżeli cyfr jednokrotnych jest tyle samo to ładniejsza jest ta liczba, która posiada mniej cyfr wielokrotnych. 
+Na przykład: liczba 123 jest ładniejsza od 455, liczba 1266 jest ładniejsza od 114577, a liczby 2344 i 67333 są jednakowo ładne. Dana jest 
+tablica T zawierająca liczby naturalne. Proszę zaimplementować funkcję: pretty_sort(T), która sortuje elementy tablicy T od najładniejszych 
+do najmniej ładnych. Użyty algorytm powinien  być  możliwie  jak  najszybszy.  
+'''
+# TODO
