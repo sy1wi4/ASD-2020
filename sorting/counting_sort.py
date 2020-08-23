89bf8dc1f5d878e95a375bb9f78d6979b6d7d43e
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-23 12:59:14 +0200

    Update counting_sort.py

--- sorting/counting_sort.py
+++ sorting/counting_sort.py
@@ -1,5 +1,6 @@
 from random import randint
 
+# porządek rosnący
 def countingSort(arr):
     n = len(arr)   
     k = max(arr)
@@ -17,6 +18,29 @@ def countingSort(arr):
         count[arr[i]] -= 1
 
     return output
+
+
+# porządek malejący
+# malejąco
+def countingSort_descending(arr):
+    n = len(arr)   
+    k = max(arr)
+    output = [0] * n      # tablica wyjsciowa tej samej wielkosci co wejsciowa
+    count = [0] * (k+1)   # potrzebujemy tablice do zliczenia na klucze od 0 do k
+
+    for i in range(n):    # dla kazdej z wartosci w tablicy zwiekszam element o odpowiednim indeksie o 1
+        count[arr[i]] += 1
+        
+    for i in range(k-1,-1,-1):                  # cumulative sum- teraz wartosc elementu o danym indeksie to ilosc elementow  
+        count[i] += count[i + 1]        # mniejszych badz rownych, co pozwoli zachowac stabilnosc algorytmu
+
+    for i in range(n-1, -1, -1):             # umieszczamy kazdy element z tablicy wejsciowej (od konca) w wyjsciowej
+        output[count[arr[i]] - 1] = arr[i]   # -1 poniewaz indeksujemy od 0
+        count[arr[i]] -= 1
+    
+    return output
+
+
 
 arr=[]
 for i in range(7):
