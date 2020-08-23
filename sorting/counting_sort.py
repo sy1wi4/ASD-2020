a11e6c61ae1da0fb14da44ac59b62fc3d6430153
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-23 13:00:31 +0200

    Update counting_sort.py

--- sorting/counting_sort.py
+++ sorting/counting_sort.py
@@ -6,10 +6,10 @@ from random import randint
 def countingSort(arr):
     n = len(arr)   
     k = max(arr)
-    output = [0] * n      #tablica wyjsciowa tej samej wielkosci co wejsciowa
-    count = [0] * (k+1)   #potrzebujemy tablice do zliczenia o wielkosci k (tyle kluczy)
+    output = [0] * n      # tablica wyjsciowa tej samej wielkosci co wejsciowa
+    count = [0] * (k+1)   # potrzebujemy tablice do zliczenia o wielkosci k (tyle kluczy)
 
-    for i in range(n):     #dla kazdej z wartosci w tablicy zwiekszam element o odpowiednim indeksie o 1
+    for i in range(n):     # dla kazdej z wartosci w tablicy zwiekszam element o odpowiednim indeksie o 1
         count[arr[i]] += 1
 
     for i in range(1,k+1):                  # cumulative sum- teraz wartosc elementu o danym indeksie to ilosc elementow  
@@ -28,17 +28,16 @@ def countingSort(arr):
 def countingSort_descending(arr):
     n = len(arr)   
     k = max(arr)
-    output = [0] * n      # tablica wyjsciowa tej samej wielkosci co wejsciowa
-    count = [0] * (k+1)   # potrzebujemy tablice do zliczenia na klucze od 0 do k
+    output = [0] * n      
+    count = [0] * (k+1)   
 
-    for i in range(n):    # dla kazdej z wartosci w tablicy zwiekszam element o odpowiednim indeksie o 1
+    for i in range(n):   
         count[arr[i]] += 1
         
-    for i in range(k-1,-1,-1):                  # cumulative sum- teraz wartosc elementu o danym indeksie to ilosc elementow  
-        count[i] += count[i + 1]        # mniejszych badz rownych, co pozwoli zachowac stabilnosc algorytmu
-
-    for i in range(n-1, -1, -1):             # umieszczamy kazdy element z tablicy wejsciowej (od konca) w wyjsciowej
-        output[count[arr[i]] - 1] = arr[i]   # -1 poniewaz indeksujemy od 0
+    for i in range(k-1,-1,-1):                   
+        count[i] += count[i + 1]       
+    for i in range(n-1, -1, -1):            
+        output[count[arr[i]] - 1] = arr[i]   
         count[arr[i]] -= 1
     
     return output
