e6829ccd3fda00c59fb773ee3d44c94a6e4d44dc
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-08-24 11:15:16 +0200

    Update coins.py

--- dynamic programming/coins.py
+++ dynamic programming/coins.py
@@ -1,12 +1,14 @@
-''' Mamy daną tablicę z nominałami monet stosowanych w pewnym dziwnym kraju, oraz kwotę T. Proszę podać algorytm,
- który oblicza minimalną ilość monet potrzebną do wydania
+''' 
+Mamy daną tablicę z nominałami monet stosowanych w pewnym dziwnym kraju, oraz kwotę T. Proszę podać algorytm,
+który oblicza minimalną ilość monet potrzebną do wydania
 kwoty T (algorytm zachłanny, wydający najpierw największą monetę, 
-nie działa: dla monet 1, 5, 8 wyda kwotę 15 jako 8 + 5 + 1 + 1 zamiast 5 + 5 + 5). '''
+nie działa: dla monet 1, 5, 8 wyda kwotę 15 jako 8 + 5 + 1 + 1 zamiast 5 + 5 + 5). 
+'''
 
 # O(T*ilosc nominalow)
 
 # zakladam ze tablica z nominalami jest posortowana rosnaco - jesli nie, to mozemy posortowac
-# zauwazmy, ze f(i)= 1 + min(f(i-nom), nom->dostepne nominaly), nasze rozwiazanie to f(n)
+# zauwazmy, ze f(i)= 1 + min(f(i-nom), nom->dostepne nominaly, nasze rozwiazanie to f(n)
 
 
 def coins(arr,T):
